6bc62e6ddb603d4a1976e5f54c4567b94db573e7
yakov matan <yakovmatan@gmail.com>
2025-05-13 11:30:45 +0300

    update case c

--- theSeriesAnalyzer/Program.cs
+++ theSeriesAnalyzer/Program.cs
@@ -131,7 +131,7 @@ namespace theSeriesAnalyzer
                         break;
 
                     case "c":
-                        //ReversSeries();
+                        ReversSeries(ints);
                         break;
 
                     case "d":
